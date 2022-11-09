2ddbf7ad4022f4660fcc161dbfe96eb0931560bf
matej-2003 <matej.kodermac1@gmail.com>
2022-11-09 18:54:03 +0100

    funkcije

--- Game.java
+++ Game.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /**
- * 
+ *
  * Game
  */
 public class Game {
@@ -37,36 +37,46 @@ public class Game {
 		gameLoop();
 	}
 
+
 	public void gameLoop() {
 		while (!checkMate) {
-			int position[] = getPostition();
-			System.out.println(position);
-
 			displayBoard();
+
+			int position[] = getPostition();
+			System.out.println(position[0] + ", " + position[1]);
+			System.out.println(displayPiece(board[position[0]][position[1]], 2, true));
 		}
 	}
 
+
 	public int[] getPostition() {
 		String pos = "";
+		int return_pos[] = new int[2];
 		do {
 			System.out.print("Enter position: ");
 			pos = sc.nextLine();
 			if (pos.length() == 2) {
-				break;
-			} else {
-				System.out.println("Enter a valid position");
+				char row = pos.charAt(0);
+				char column = pos.charAt(1);
+				if (("abcdefgh").contains(row + "") && ("12345678").contains(column + "")) {
+					return_pos[0] = Integer.parseInt(column + "") - 1;
+					return_pos[1] = Game.getRow(row);
+					break;
+				}
 			}
+			System.out.println("Enter a valid position");
 		} while (true);
-		return new int[1];
+		return return_pos;
 	}
-	
+
+
 	public void displayBoard() {
 		System.out.println("  +------+------+------+------+------+------+------+------+");
 		for (int y = 0; y < board.length; y++) {
-			System.out.print(y + " |");
+			System.out.print(y + 1 + " |");
 			for (int x = 0; x < board.length; x++) {
 				boolean isWhite = x % 2 == 1;
-				if (y % 2 == 0) isWhite = x % 2 == 0; 
+				if (y % 2 == 0) isWhite = x % 2 == 0;
 				System.out.print(displayPiece(board[y][x], 6, isWhite) + "|");
 			}
 			System.out.println();
@@ -75,11 +85,12 @@ public class Game {
 		System.out.println("     a      b      c      d      e      f      g      h   ");
 	}
 
+
 	public String displayPiece(int piece_code, int length, boolean white) {
 		String piece_string = "";
 		int buffer_len = (length - 2)/ 2;
 		String white_space = " ";
-		if (white) white_space = "▒";
+		if (white) white_space = " "; //"▒";
 
 		switch (piece_code) {
 			case BPawn:
@@ -123,6 +134,16 @@ public class Game {
 		}
 		return  white_space.repeat(buffer_len) + piece_string + white_space.repeat(buffer_len);
 	}
+
+
+	public static int getRow(char row) {
+		String row_symbols = "abcdefgh";
+		for (int i = 0; i < row_symbols.length(); i++) {
+			if (row == row_symbols.charAt(i)) return i;
+		}
+		return 0;
+	}
+
 
 	public static void main(String[] args)  {
 		new Game();
